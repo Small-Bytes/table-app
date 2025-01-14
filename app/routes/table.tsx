ecf4be521d3c8e55fff628b0ab3e14204b057270
SandSeb <sebsk@proton.me>
2025-01-14 19:12:46 +0100

    change to Server Side and and postgres db

--- app/routes/table.tsx
+++ app/routes/table.tsx
@@ -1,16 +1,16 @@
-import type { Route } from "./+types/table";
+import { useLoaderData } from 'react-router';
+import { sql } from '~/db.server';
 
-export function clientLoader() {
-  return { header: 'Resultat' }
-}
+export const loader = async () => {
+  // console.log(params);
+  // if (!params.sharableKey) {
+  //   throw new Response("Not Found", { status: 404 });
+  // }
+  const response = await sql`select * from tableResults`;
+  return response[1].data.players;
+};
 
-export default function TableData({
-  loaderData,
-}: Route.ComponentProps) {
-
-    return (
-    <div>
-        <h1>{loaderData.header}</h1>
-        <p>Sist: Kvist har aldrig vunnit något</p>
-    </div>)
+export default function Page() {
+  const data = useLoaderData();
+  return <>{JSON.stringify(data, null, 2)}</>;
 }
